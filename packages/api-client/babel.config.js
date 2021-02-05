ffd50ca4181b4e36943c245207147b4f5558db96
Benny Neugebauer <bn@bennyn.de>
2021-02-05 18:07:16 +0100

    Temporarily remove api-client

--- packages/api-client/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: [
-    '@babel/plugin-proposal-optional-chaining',
-    '@babel/plugin-proposal-class-properties',
-    '@babel/plugin-proposal-object-rest-spread',
-  ],
-  presets: ['@babel/preset-react', '@babel/preset-typescript'],
-};
