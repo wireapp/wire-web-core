e8fc745d49bf16c312b74cafaf2111b5ee4ec1fa
Benny Neugebauer <bn@bennyn.de>
2021-04-22 11:00:59 +0200

    add license header plugin

--- packages/cryptobox/karma.conf.ts
+++ packages/cryptobox/karma.conf.ts
@@ -16,6 +16,7 @@
  * along with this program. If not, see http://www.gnu.org/licenses/.
  *
  */
+
 import type {Config} from 'karma';
 import webpackConfig from './webpack.config';
 import {ConfigOptions} from 'karma';
