c1dc5508e98f384122af42243f94635da83960e6
Benny Neugebauer <bn@bennyn.de>
2021-06-24 10:18:17 +0200

    feat(cryptobox-worker): Export Cryptobox Worker

--- packages/cryptobox-worker/src/CryptoboxWorkerExport.js
+++ packages/cryptobox-worker/src/CryptoboxWorkerExport.js
@@ -17,10 +17,14 @@
  *
  */
 
-import {getCryptoboxWorker} from './CryptoboxWorkerExport.js';
+import {Worker} from 'worker_threads';
+import {wrap} from 'comlink';
+// @ts-ignore
+import nodeEndpoint from 'comlink/dist/esm/node-adapter.mjs';
 
-(async () => {
-  const cryptobox = await getCryptoboxWorker();
-  const result = await cryptobox.fingerprint();
-  console.info(result);
-})().catch(console.error);
+export async function getCryptoboxWorker() {
+  const inBrowser = typeof process !== 'object';
+  const workerConstructor = inBrowser ? Worker : (await import('worker_threads')).Worker;
+  const worker = new workerConstructor('./src/CryptoboxWorker.js');
+  return wrap(inBrowser ? worker : nodeEndpoint(worker));
+}
