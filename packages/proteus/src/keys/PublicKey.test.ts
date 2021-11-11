e17cfa0d66bc23dbc9a3f46eb1d28f616d9c83c8
VictorWissink <victor-wissink@hotmail.com>
2021-11-11 22:45:11 +0100

    Add subsystem tags for Cryptobox testcases

--- packages/proteus/src/keys/PublicKey.test.ts
+++ packages/proteus/src/keys/PublicKey.test.ts
@@ -23,7 +23,7 @@ import {SecretKey} from './SecretKey';
 describe('Public Key', () => {
 
   // This test conforms to the following testing standards:
-  // @SF.Messages @TSFI.RESTfulAPI
+  // @SF.Messages @TSFI.RESTfulAPI @S0.2 @S0.3 
   it('rejects shared secrets at the point of infinity', async () => {
     try {
       const emptyCurve = new Uint8Array([1].concat(Array.from({length: 30})));
